6d6cb089aa5e0583ed30ef166282030a767523c7
avimallik <armavicse@gmail.com>
2022-09-26 21:07:18 +0600

    initial commit

---
+++ app/src/main/java/com/enigmarobolox/arm_avi/enigmarobolox/TestUI.java
@@ -0,0 +1,13 @@
+package com.enigmarobolox.arm_avi.enigmarobolox;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TestUI extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_u_i);
+    }
+}
